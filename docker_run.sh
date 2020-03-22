abc5db5228b7e69c8f712a970411888118c0578a
Patryk Las <patlas92@wp.pl>
2020-03-22 11:03:30 +0100

    Fix docker image address

---
+++ docker_run.sh
@@ -0,0 +1,8 @@
+docker run -it \
+--network="host" \
+-e "MQTT_SERVER=mqtt://127.0.0.1" \
+-e "MQTT_BASE_TOPIC=ewpe-smart" \
+-e "NETWORK=192.168.1.255" \
+-e "DEVICE_POLL_INTERVAL=5000" \
+--name ewpe-smart-mqtt \
+demydiuk/ewpe-smart-mqtt:latest
