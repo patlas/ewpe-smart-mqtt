ba0c83043110b57e18f59ada2579738666dba8bb
patlas <patlas92@wp.pl>
2020-03-29 11:08:36 +0200

    Fix paths

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,5 @@
-docker rm ewpe-smart-mqtt
-docker run  -it \
+/usr/bin/docker rm ewpe-smart-mqtt
+/usr/bin/docker run  -it \
 --network="host" \
 -e "MQTT_SERVER=mqtt://127.0.0.1" \
 -e "MQTT_BASE_TOPIC=ewpe-smart" \
